944e64d14d50e7be0e0a222ea04c1d2808f75c06
idikim <hjkim9217@naver.com>
2024-12-09 00:31:07 +0900

    add: test 파일 추가 및 테스트 완료

---
+++ test/location_model_test.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:flutter_local_search_app/data/model/location.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  String jsonString = """
+    {
+      "title": "<b>코엑스</b>",
+      "link": "http://www.coex.co.kr/",
+      "category": "문화,예술>컨벤션센터",
+      "description": "",
+      "telephone": "",
+      "address": "서울특별시 강남구 삼성동 159",
+      "roadAddress": "서울특별시 강남구 영동대로 513",
+      "mapx": "1270594274",
+      "mapy": "375116620"
+    }
+""";
+
+  test(
+    'Location model convert test',
+    () {
+      Map<String, dynamic> map = jsonDecode(jsonString);
+      Location location = Location.fromJson(map);
+
+      expect(location.roadAddress, "서울특별시 강남구 영동대로 513");
+    },
+  );
+}
