b0acfc06ad155d8b757692c9fcb851d706be6eee
idikim <hjkim9217@naver.com>
2024-12-09 17:16:09 +0900

    feat: HomeViewModel 구현

---
+++ lib/ui/pages/home/home_view_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_local_search_app/data/model/location.dart';
+import 'package:flutter_local_search_app/data/repository/location_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeState {
+  HomeState({required this.locations});
+  List<Location>? locations;
+}
+
+class HomeViewModel extends Notifier<HomeState> {
+  @override
+  HomeState build() {
+    return HomeState(locations: null);
+  }
+
+  Future<void> search(String query) async {
+    LocationRepository locationRepository = LocationRepository();
+    state = HomeState(
+      locations: await locationRepository.search(query),
+    );
+  }
+}
+
+final homeViewModelProvider = NotifierProvider<HomeViewModel, HomeState>(
+  () {
+    return HomeViewModel();
+  },
+);
