3da42ac3f650ef0b3e20aba510c5b6ff5a52595b
idikim <hjkim9217@naver.com>
2024-12-09 21:05:38 +0900

    fix: 코드작업을 위해 homepage에 리스트뷰를 직접넣음

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_search_app/data/model/location.dart';
+import 'package:flutter_local_search_app/ui/pages/detail/detail_page.dart';
+import 'package:flutter_local_search_app/ui/pages/home/home_view_model.dart';
 import 'package:flutter_local_search_app/ui/pages/home/widgets/home_list_view.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerStatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  ConsumerState<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends ConsumerState<HomePage> {
   TextEditingController textEditingController = TextEditingController();
 
   void search(String text) {
+    ref.read(homeViewModelProvider.notifier).search(text);
     print("search");
   }
 
@@ -23,6 +28,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    HomeState homeState = ref.watch(homeViewModelProvider);
+
     return GestureDetector(
       onTap: () {
         // 3. UX 고려하기
@@ -76,7 +83,78 @@ class _HomePageState extends State<HomePage> {
           padding: EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [HomeListView()],
+            children: [
+              Expanded(
+                child: ListView.separated(
+                  itemCount: homeState.locations?.length ?? 0,
+                  separatorBuilder: (context, index) => SizedBox(
+                    height: 20,
+                  ),
+                  itemBuilder: (context, index) {
+                    Location location = homeState.locations![index];
+                    return Builder(
+                      builder: (context) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return DetailPage();
+                                },
+                              ),
+                            );
+                          },
+                          child: SizedBox(
+                            height: 120,
+                            width: double.infinity,
+                            child: Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(
+                                    color: Colors.black12,
+                                  )),
+                              padding: EdgeInsets.all(20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '삼성1동 주민센터',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  Text(
+                                    '공공,사회기관>행정복지센터',
+                                    style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  Text(
+                                    '서울특별시 강남구 봉은사로 616 삼성1동 주민센터',
+                                    style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 14,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
